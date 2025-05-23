aa94ab4868f9bb2a0aa0d8dc3045924692a73033
cha0s <git@cha0s.io>
2025-05-23 09:36:01 -0500

    fix: tests

--- src/query.test.js
+++ src/query.test.js
@@ -1,68 +1,68 @@
 import {expect, test} from 'vitest';
 
-// import Entity from './entity.js';
-// import Query from './query.js';
-// import {fakeEnvironment} from './test-helper.js';
+import Entity from './entity.js';
+import Query from './query.js';
+import {fakeEnvironment} from './test/components.js';
 
-// const {A, C, one, two, three} = fakeEnvironment();
+const {A, C, one, two, three} = fakeEnvironment();
 
-// function expectQuery(parameters, expected) {
-//   const query = new Query(parameters);
-//   query.reindex(one);
-//   query.reindex(two);
-//   query.reindex(three);
-//   expect(Array.from(query.select()).map(({id}) => id)).to.deep.equal(expected);
-// }
+function expectQuery(parameters, expected) {
+  const query = new Query(parameters);
+  query.reindex(one);
+  query.reindex(two);
+  query.reindex(three);
+  expect(Array.from(query.select()).map(({id}) => id)).to.deep.equal(expected);
+}
 
 test('query all', () => {
-//   expectQuery([], [1, 2, 3]);
+  expectQuery([], [1, 2, 3]);
 });
 
-// test('query some', () => {
-//   expectQuery(['A'], [2, 3]);
-//   expectQuery(['A', 'B'], [2]);
-// });
+test('query some', () => {
+  expectQuery(['A'], [2, 3]);
+  expectQuery(['A', 'B'], [2]);
+});
 
-// test('query excluding', () => {
-//   expectQuery(['!A'], [1]);
-//   expectQuery(['A', '!B'], [3]);
-// });
+test('query excluding', () => {
+  expectQuery(['!A'], [1]);
+  expectQuery(['A', '!B'], [3]);
+});
 
-// test('deindex', () => {
-//   const query = new Query(['A']);
-//   query.reindex(one);
-//   query.reindex(two);
-//   query.reindex(three);
-//   expect(query.count).to.equal(2);
-//   query.deindex(two);
-//   expect(query.count).to.equal(1);
-// });
+test('deindex', () => {
+  const query = new Query(['A']);
+  query.reindex(one);
+  query.reindex(two);
+  query.reindex(three);
+  expect(query.count).to.equal(2);
+  query.deindex(two);
+  expect(query.count).to.equal(1);
+});
 
-// test('reindex', () => {
-//   const query = new Query(['B']);
-//   query.reindex(one);
-//   query.reindex(two);
-//   expect(query.count).to.equal(2);
-//   const newTwo = new Entity(2);
-//   newTwo.addComponent(A);
-//   newTwo.addComponent(C);
-//   query.reindex(newTwo);
-//   expect(query.count).to.equal(1);
-// });
+test('reindex', () => {
+  const query = new Query(['B']);
+  query.reindex(one);
+  query.reindex(two);
+  expect(query.count).to.equal(2);
+  const newTwo = new Entity(2);
+  newTwo.addComponent(A);
+  newTwo.addComponent(C);
+  query.reindex(newTwo);
+  expect(query.count).to.equal(1);
+});
 
-// test('select', () => {
-//   const query = new Query(['A']);
-//   query.reindex(one);
-//   query.reindex(two);
-//   query.reindex(three);
-//   const it = query.select();
-//   let result;
-//   result = it.next();
-//   expect(result.done).to.be.false;
-//   expect(result.value.id).to.equal(2);
-//   result = it.next();
-//   expect(result.done).to.be.false;
-//   expect(result.value.id).to.equal(3);
-//   result = it.next();
-//   expect(result.done).to.be.true
-// });
+test('select', () => {
+  const query = new Query(['A']);
+  query.reindex(one);
+  query.reindex(two);
+  query.reindex(three);
+  const it = query.select();
+  let result;
+  result = it.next();
+  expect(result.done).to.be.false;
+  expect(result.value.id).to.equal(2);
+  result = it.next();
+  expect(result.done).to.be.false;
+  expect(result.value.id).to.equal(3);
+  result = it.next();
+  expect(result.done).to.be.true
+});
--- src/test/components.js
+++ src/test/components.js
@@ -1,4 +1,5 @@
 import Component from '../component.js';
+import Entity from '../entity.js';
 
 export class Position extends Component {
   static properties = {
@@ -6,3 +7,42 @@ export class Position extends Component {
     y: {type: 'uint8'},
   };
 }
+
+export function wrapComponents(Components) {
+  return Components
+    .reduce((Components, [componentName, properties]) => {
+      return {
+        ...Components,
+        [componentName]: class extends Component {
+          static componentName = componentName;
+          static properties = properties;
+        },
+      };
+    }, {});
+}
+
+export function fakeEnvironment() {
+  const Components = wrapComponents([
+    ['A', {a: {type: 'int32', defaultValue: 64}}],
+    ['B', {b: {type: 'int32', defaultValue: 32}}],
+    ['C', {c: {type: 'int32'}}],
+    ['D', {d: {type: 'float64'}, e: {type: 'float64'}}],
+    ['E', {e: {type: 'object', properties: {f: {type: 'int32'}, g: {type: 'int32'}}}}],
+  ]);
+  const {A, B, C, D, E} = Components;
+  A.storage = new A.Storage(Components.A);
+  B.storage = new C.Storage(Components.B);
+  C.storage = new A.Storage(Components.C);
+  D.storage = new A.Storage(Components.D);
+  E.storage = new A.Storage(Components.E);
+  const one = new Entity(1);
+  one.addComponent(B);
+  const two = new Entity(2);
+  two.addComponent(A);
+  two.addComponent(B);
+  two.addComponent(C);
+  const three = new Entity(3);
+  three.addComponent(A);
+  return {A, B, C, D, E, Components, one, two, three};
+}
+
