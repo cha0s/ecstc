99ffb839fee96c421f18f4792c93bdecf27220bf
cha0s <git@cha0s.io>
2025-05-24 11:30:22 -0500

    fix: ids

--- bench/entities.js
+++ bench/entities.js
@@ -7,14 +7,14 @@ const world = new World({Components: ComponentRegistry});
 const entities = Array(10000);
 for (let j = 0; j < 10; ++j) {
   for (let i = 0; i < entities.length; ++i) {
-    entities[i] = world.createSpecific(i, {Position: {x: 1.0}});
+    entities[i] = world.createSpecific(i + 1, {Position: {x: 1.0}});
   }
 }
 
 let start;
 start = performance.now();
 for (let i = 0; i < entities.length; ++i) {
-  entities[i] = world.createSpecific(i, {Position: {x: 1}});
+  entities[i] = world.createSpecific(i + 1, {Position: {x: 1}});
 }
 console.log(entities.length, performance.now() - start)
 
