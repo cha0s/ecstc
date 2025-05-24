d4aae714e4689e1cdd30fbed36e3b3218cfdd6b3
cha0s <git@cha0s.io>
2025-05-24 05:54:27 -0500

    fix: bench

--- bench/entities.js
+++ bench/entities.js
@@ -15,10 +15,10 @@ console.log(entities.length, performance.now() - start)
 start = performance.now();
 for (let i = 0; i < entities.length; ++i) {
   if (i % 2) {
-    entities[i].x = Math.random();
+    entities[i].Position.x = Math.random();
   }
   else {
-    entities[i].y = Math.random();
+    entities[i].Position.y = Math.random();
   }
 }
 console.log(entities.length, performance.now() - start)
