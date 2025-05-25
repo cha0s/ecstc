612bdb5dcbe33b2cc10963e6bed5d268b4421aaa
cha0s <git@cha0s.io>
2025-05-24 23:09:49 -0500

    dev: bench

--- bench/entities.js
+++ bench/entities.js
@@ -4,7 +4,7 @@ import World from '../src/world.js';
 import '../src/test/components.js';
 
 const world = new World({Components: ComponentRegistry});
-const entities = Array(50000);
+const entities = Array(10000);
 for (let j = 0; j < 10; ++j) {
   for (let i = 0; i < entities.length; ++i) {
     entities[i] = world.createSpecific(i + 1, {Position: {x: 1.0}});
@@ -12,11 +12,17 @@ for (let j = 0; j < 10; ++j) {
 }
 
 let start;
+function measure(label) {
+  console.log(
+    `\x1b[33m${((performance.now() - start) / entities.length * 1000).toFixed(4)}\x1b[0mμs`,
+    label,
+  );
+}
 start = performance.now();
 for (let i = 0; i < entities.length; ++i) {
   entities[i] = world.createSpecific(i + 1, {Position: {x: 1}});
 }
-console.log(entities.length, performance.now() - start)
+measure('create');
 
 const values = Array(entities.length).fill(0).map(() => Math.random());
 start = performance.now();
@@ -28,6 +34,11 @@ for (let i = 0; i < entities.length; ++i) {
     entities[i].Position.y = values[i];
   }
 }
-console.log(entities.length, performance.now() - start)
+measure('set properties');
 
-// console.log(world.diff())
+const positions = Array(entities.length);
+start = performance.now();
+for (let i = 0; i < entities.length; ++i) {
+  positions[i] = entities[i].toJSONWithoutDefaults();
+}
+measure('without defaults');
