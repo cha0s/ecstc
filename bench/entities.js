1d14f584a4a58dca0e23b9dbd5a9cff0c8749828
cha0s <git@cha0s.io>
2025-05-31 13:18:48 -0500

    fix: bench

--- bench/entities.js
+++ bench/entities.js
@@ -5,9 +5,9 @@ import {Components} from '../src/testing.js';
 const {Position} = Components;
 
 const world = new World({Components: {Position}});
-const entities = Array(1000);
+const N = 50000;
+const entities = Array(N);
 const positions = Array(entities.length);
-const values = Array(entities.length).fill(0).map(() => Math.random());
 
 function create() {
   for (let i = 0; i < entities.length; ++i) {
@@ -18,10 +18,10 @@ function create() {
 function setProperties() {
   for (let i = 0; i < entities.length; ++i) {
     if (0 === (i & 1)) {
-      entities[i].Position.x = values[i];
+      entities[i].Position.x = i;
     }
     else {
-      entities[i].Position.y = values[i];
+      entities[i].Position.y = i;
     }
   }
 }
@@ -29,7 +29,15 @@ function setProperties() {
 const LocalPosition = world.Components.Position;
 function directSetProperties() {
   for (const {dirty, view} of LocalPosition.pool.chunks) {
-    new Float32Array(view.buffer).set(values);
+    const array = new Float32Array(view.buffer);
+    for (let i = 0, j = 0; i < array.length; ++i, j += 2) {
+      if (0 === (i & 1)) {
+        array[j] = i;
+      }
+      else {
+        array[j + 1] = i;
+      }
+    }
     dirty.fill(~0);
   }
 }
@@ -41,7 +49,7 @@ function withoutDefaults() {
 }
 
 // warm up ICs
-for (let j = 0; j < 100; ++j) {
+for (let j = 0; j < 100000 / N; ++j) {
   create();
   setProperties();
   directSetProperties();
@@ -52,11 +60,15 @@ await new Promise((resolve) => setTimeout(resolve, 1000));
 
 let start;
 function measure(label) {
+  const ms = performance.now() - start;
   console.log(
-    `\x1b[33m${((performance.now() - start) / entities.length * 1000).toFixed(4)}\x1b[0mμs`,
+    `\x1b[33m${ms}ms (${(ms / entities.length * 1000).toFixed(4)}\x1b[0mμs/op)`,
     label,
   );
 }
+
+console.log(N, 'entities');
+
 start = performance.now();
 create();
 measure('create');
